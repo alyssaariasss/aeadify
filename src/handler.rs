94f87bef91a463e40d5f3ce37152868dee440b41
Alyssa <alyssaarias27@gmail.com>
2024-05-15 22:20:22 +0800

    update documentation comments in handler.rs

--- src/handler.rs
+++ src/handler.rs
@@ -7,6 +7,7 @@ use orion::{
     kex::SecretKey,
 };
 
+/// Model to hold generated SecretKey and Salt for passphrase.
 #[derive(Debug)]
 struct KeySalt {
     key: SecretKey,
@@ -16,7 +17,15 @@ struct KeySalt {
 /// Derives a password using the decoded passphrase and internally adds a default salt.
 ///
 /// It then derives the SecretKey from the passphrase and the generated salt using a password-hashing function Argon2i.
-fn stretch_passphrase(passphrase: &String) -> KeySalt {
+///
+/// # Parameters
+///
+/// `passphrase` - String that serves as the user's password.
+///
+/// # Returns
+///
+/// Returns the generated SecretKey and salt represented by the `KeySalt` struct.
+fn derive_key_with_random_salt(passphrase: &String) -> KeySalt {
     let user_password = Password::from_slice(passphrase.as_bytes()).unwrap();
     let salt = Salt::default();
 
@@ -27,50 +36,99 @@ fn stretch_passphrase(passphrase: &String) -> KeySalt {
 /// Derives a password using the decoded passphrase and externally added salt (from slice of encrypted string).
 ///
 /// It then derives the SecretKey from the passphrase and the generated salt using a password-hashing function Argon2i.
-fn stretch_passphrase_with_salt(passphrase: &String, salt: &[u8]) -> SecretKey {
+///
+/// # Parameters
+///
+/// `passphrase` - String that serves as the user's password.
+/// `salt` - Randomly generated salt.
+///
+/// # Returns
+///
+/// Returns the stretched passphrase with salt as a `SecretKey`.
+fn derive_key_with_custom_salt(passphrase: &String, salt: &[u8]) -> SecretKey {
     let salt = Salt::from_slice(salt).unwrap();
     let user_password = Password::from_slice(passphrase.as_bytes()).unwrap();
     derive_key(&user_password, &salt, 3, 1 << 16, 32).unwrap()
 }
 
-/// Encrypts the given string bytes using password and returns it as a base64 alphabet.
+/// Encrypts the given string bytes using password.
+///
+/// # Parameters
+///
+/// `string_as_bytes` - Content to be encrypted that is converted into bytes.
+///
+/// `password` - String that serves as the user's password.
+///
+/// # Returns
+///
+/// Returns the encrypted string as base64.
 fn enrypt_string(
     string_as_bytes: &[u8],
     password: &String,
 ) -> Result<String, Box<dyn std::error::Error>> {
-    let passphrase = stretch_passphrase(password);
+    let passphrase = derive_key_with_random_salt(password);
     let ciphertext = seal(&passphrase.key, string_as_bytes)?;
     let cipertext_with_salt = [passphrase.salt.as_ref(), &ciphertext].concat();
     encode_to_base64(&cipertext_with_salt)
 }
 
 /// Encodes bytes as base64 string.
+///
+/// # Parameters
+///
+/// `encrypted_bytes` - Encrypted content that is converted into bytes.
+///
+/// # Returns
+///
+/// Returns the base64 String of `encrypted_bytes`.
 fn encode_to_base64(encrypted_bytes: &Vec<u8>) -> Result<String, Box<dyn std::error::Error>> {
     Ok(BASE64_STANDARD.encode(encrypted_bytes))
 }
 
 /// Decodes base64 string as bytes.
+///
+/// # Parameters
+///
+/// `encoded_base64` - Encoded base64 content of the encrypted string.
+///
+/// # Returns
+///
+/// Returns the resulting bytes of `encoded_base64`.
 fn decode_base64(encoded_base64: &String) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     Ok(BASE64_STANDARD.decode(encoded_base64)?)
 }
 
 /// Decrypts a base64 string using password and returns it as a String.
+///
+/// # Parameters
+///
+/// `encoded_base64` - Encoded base64 content of the encrypted string.
+///
+/// `password` - String that serves as the user's password.
+///
+/// # Returns
+///
+/// Returns the decrypted cipherstring of `encoded_base64`.
 fn decrypt_cipherstring(
     encoded_base64: &String,
     password: &String,
 ) -> Result<String, Box<dyn std::error::Error>> {
     let cipherstring = decode_base64(encoded_base64)?;
     let (salt, cipherstring) = cipherstring.split_at(16);
-    let passphrase = stretch_passphrase_with_salt(password, salt);
+    let passphrase = derive_key_with_custom_salt(password, salt);
     let decrypted_text = open(&passphrase, cipherstring)?;
     Ok(String::from_utf8(decrypted_text)?)
 }
 
 /// Stores encrypted content in the `encrypted` directory.
 ///
-/// This function requires a borrowed reference to the key name `key`, the content to be encrypted, and the password.
+/// # Parameters
 ///
-/// It then stores the key-value pair at the `encrypted` folder.
+/// `key` - Borrowed reference to the key name.
+///
+/// `content` - Content to be encrypted.
+///
+/// `password` - User-input password
 pub fn store_keys(
     key: &String,
     content: &String,
@@ -93,9 +151,15 @@ pub fn store_keys(
 
 /// Returns decrypted content from the `encrypted` directory.
 ///
-/// This function requires a borrowed reference to the key name `key`, and the password used for encryption.
+/// # Parameters
 ///
-/// It reads the text from the corresponding directory, decrypts the text using the passphrase, and returns it as a `String`.
+/// `key` - Borrowed reference to the key name.
+///
+/// `password` - User-input password
+///
+/// # Returns
+///
+/// Returns the retrieved and decrypted content into String.
 pub fn get_keys(key: &String, password: &String) -> Result<String, Box<dyn std::error::Error>> {
     let dir_path = Path::new("./encrypted");
     let tree = sled::open(dir_path)?;
@@ -105,6 +169,6 @@ pub fn get_keys(key: &String, password: &String) -> Result<String, Box<dyn std::
         let value = decrypt_cipherstring(&raw_value, password)?;
         Ok(value)
     } else {
-        Err("key not found".into())
+        Err("Key not found".into())
     }
 }
